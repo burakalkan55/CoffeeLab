086776b6d87f3476823361ffb0745600e3df3250
burakalkan55 <alkanburak131@gmail.com>
2025-11-18 01:19:51 +0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-white">
+      <div className="p-10 rounded-2xl shadow-lg border">
+        <h1 className="text-4xl font-bold mb-4">☕ CoffeeLab</h1>
+        <p className="text-gray-600">Next.js + TypeScript + Tailwind başarıyla kuruldu!</p>
+      </div>
+    </main>
+  );
+}
